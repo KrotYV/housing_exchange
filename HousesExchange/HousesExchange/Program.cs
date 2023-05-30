832d913a391ecab2060b83bbf950db65bfb49af5
Yulia <87913570+KrotYV@users.noreply.github.com>
2023-05-30 22:34:36 +0200

    add class House with parameters

--- HousesExchange/HousesExchange/Program.cs
+++ HousesExchange/HousesExchange/Program.cs
@@ -1,3 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HousesExchange
+{
+    public class House
+    {
+        public string District { get; set; }
+        public double Area { get; set; }
+        public string Layout { get; set; }
+        public int Floor { get; set; }
+        public int NumberOfRooms { get; set; }
+        public List<string> Requirements { get; set; }
 
+        public House(string district, double area, string layout, List<string> requirements, int roomCount, int floor)
+    }
+}
